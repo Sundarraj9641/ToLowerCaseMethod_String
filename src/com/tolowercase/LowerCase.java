07e1696ae71f33f4e1fa91e85003ea82b00b92f8
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-19 11:04:05 +0530

    pushed by sundarraj'

---
+++ src/com/tolowercase/LowerCase.java
@@ -0,0 +1,14 @@
+package com.tolowercase;
+
+public class LowerCase {
+
+	public static void main(String[] args)
+    {
+        String str = "GET THE FUTURE YOU WANT";
+        
+        String lower = str.toLowerCase();
+
+        System.out.println("Entered String: " + str);
+        System.out.println("Lowercase String: " + lower);
+    }
+}
